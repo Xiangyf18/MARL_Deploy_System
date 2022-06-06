d1968135d2804c5a24036472857bb6e8f9499b59
Yunfei Xiang <xiangyf18@mails.tsinghua.edu.cn>
2022-06-06 11:15:04 +0800

    update codes

--- host_backend/src/nics_robot_client/scripts/robot_client_node.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-from robot_client_pypkg.robot_client import RobotClient
-import os 
-
-if __name__ == '__main__':
-    car_id = os.getenv('CAR_ID')
-    client = RobotClient(car_id)
